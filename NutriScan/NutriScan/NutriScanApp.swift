e71c01d86355d81e6d1f788504e99def8ce5752f
Youssef Abd El-Fatah <99736061+YoussefAbdel-Fatah@users.noreply.github.com>
2026-07-13 17:19:12 -0700

    first commit

---
+++ NutriScan/NutriScan/NutriScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriScanApp.swift
+//  NutriScan
+//
+//  Created by Youssef Abd El-Fatah on 13/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct NutriScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
