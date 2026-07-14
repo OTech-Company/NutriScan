3f79474d2b8db02921c609f29fcb55b72a5319ea
Youssef Abd El-Fatah <99736061+YoussefAbdel-Fatah@users.noreply.github.com>
2026-07-14 18:26:35 +0300

    Clean up comments in HTTPMethod.swift
    Removed duplicate comments and cleaned up the file.

--- NutriScan/NutriScan/Core/Network/HTTPMethod.swift
+++ NutriScan/NutriScan/Core/Network/HTTPMethod.swift
@@ -5,14 +5,6 @@
 //  Created by Youssef Abd El-Fatah on 14/07/2026.
 //
 
-
-//
-//  HTTPMethod.swift
-//  Drape
-//
-//  Created by Moaz on 01/07/2026.
-//
-
 enum HTTPMethod: String {
     case get = "GET"
     case post = "POST"
