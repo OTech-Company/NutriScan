f7f103718e16a43c7f9efa39cc2f6bb229b87615
A7mednage71 <ahmdnajkh71@gmail.com>
2026-07-15 19:38:38 +0300

    feat: implement  custom validation logic

---
+++ NutriScan/NutriScan/Core/Helpers/AppValidator.swift
@@ -0,0 +1,93 @@
+//
+//  AppValidator.swift
+//  NutriScan
+//
+//  Created by Ahmed Nageh on 15/07/2026.
+//
+
+import Foundation
+
+struct AppValidator {
+    
+    static func displayNameValidator(_ displayName: String?) -> String? {
+        guard let displayName = displayName, !displayName.isEmpty else {
+            return "Full name is required"
+        }
+        if displayName.count < 3 || displayName.count > 20 {
+            return "Full name must be between 3 and 20 characters"
+        }
+        return nil
+    }
+
+    static func validateMobile(_ value: String?) -> String? {
+        guard let value = value, !value.isEmpty else {
+            return "Mobile number is required"
+        }
+        if value.count != 11 {
+            return "Mobile number must be 11 digits"
+        }
+        let pattern = #"^(010|011|012|015)\d{8}$"#
+        let regexPredicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+        if !regexPredicate.evaluate(with: value) {
+            return "Invalid mobile number"
+        }
+        return nil
+    }
+
+    static func validateVerifyCode(_ value: String?) -> String? {
+        guard let value = value, !value.isEmpty else {
+            return "Verification code is required"
+        }
+
+        if value.count != 6 {
+            return "Verification code must be 6 digits"
+        }
+
+        let pattern = #"^[0-9]{6}$"#
+        let regexPredicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+        if !regexPredicate.evaluate(with: value) {
+            return "Invalid verification code"
+        }
+
+        return nil
+    }
+
+    static func passwordValidator(_ value: String?) -> String? {
+        guard let value = value, !value.isEmpty else {
+            return "Password is required"
+        }
+        if value.count < 6 {
+            return "Password must be at least 6 characters"
+        }
+        return nil
+    }
+
+    static func repeatPasswordValidator(value: String?, password: String?) -> String? {
+        if let validationError = passwordValidator(value) {
+            return validationError
+        }
+        if value != password {
+            return "Passwords do not match"
+        }
+        return nil
+    }
+
+    static func emailValidator(_ value: String?) -> String? {
+        guard let value = value, !value.isEmpty else {
+            return "Email is required"
+        }
+        let pattern = #"^(([^<>()\[\]\\.,;:\s@\"]+(\.[^<>()\[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$"#
+        let regexPredicate = NSPredicate(format: "SELF MATCHES %@", pattern)
+        if !regexPredicate.evaluate(with: value) {
+            return "Invalid email address"
+        }
+        return nil
+    }
+
+    static func validateRequiredTextField(_ value: String?) -> String? {
+        guard let value = value, !value.isEmpty else {
+            return "This field is required"
+        }
+        return nil
+    }
+}
