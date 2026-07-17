df3b8dd1ebdb16aa795b5e1cf6b48eeb16c98aac
osama hosam abdellatif <osama.abdellatif.official@hotmail.com>
2026-07-17 20:11:11 +0300

    setting up the feature struchure

---
+++ NutriScan/NutriScan/Features/ProfileSetup/Presentation/Routing/ProfileSetupFlowView.swift
@@ -0,0 +1,23 @@
+//
+//  HomeFlowView.swift
+//  NutriScan
+//
+//  Created by Osama Hosam on 14/07/2026.
+//
+
+
+import SwiftUI
+
+//struct HomeFlowView: View {
+//    @StateObject private var router = AppRouter()
+//
+//    var body: some View {
+//        NavigationStack(path: $router.path) {
+//            HomeView()
+//                .navigationDestination(for: AnyRoute.self) { route in
+//                    route.view()
+//                }
+//        }
+//        .environmentObject(router)
+//    }
+//}
---
+++ NutriScan/NutriScan/Features/ProfileSetup/Presentation/Routing/ProfileSetupRoute.swift
@@ -0,0 +1,22 @@
+//
+//  HomeRoute.swift
+//  NutriScan
+//
+//  Created by Osama Hosam on 14/07/2026.
+//
+//import SwiftUI
+//
+//enum HomeRoute: Route {
+//    case mealDetail(id: String)
+//    case summary
+//
+//    @ViewBuilder
+//    var destination: some View {
+//        switch self {
+//        case .mealDetail(let id):
+//            MealDetailView(mealId: id)
+//        case .summary:
+//            HomeSummaryView()
+//        }
+//    }
+//}
