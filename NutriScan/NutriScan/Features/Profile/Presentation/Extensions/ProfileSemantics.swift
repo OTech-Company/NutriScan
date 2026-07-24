0a9d16ace32997c030bdd78d1b904d8166febd01
Mina_Wagdy <minawagdy2228@gamil.com>
2026-07-25 00:27:45 +0300

    adjusted container corner radius

--- NutriScan/NutriScan/Features/Profile/Presentation/Extensions/ProfileSemantics.swift
+++ NutriScan/NutriScan/Features/Profile/Presentation/Extensions/ProfileSemantics.swift
@@ -28,7 +28,7 @@ enum ProfileSemantics {
     }
 
     enum Radius {
-        static let containerTop: CGFloat = 42
+        static let containerTop: CGFloat = 24
         static let streakBadge: CGFloat = 8
         static let familyMembersContainerLight: CGFloat = 16
         static let familyMembersContainerDark: CGFloat = 22
