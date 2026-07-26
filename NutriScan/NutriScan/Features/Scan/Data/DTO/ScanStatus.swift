0408785e84c2eff18d71a3ba000b68010463c800
Mina_Wagdy <minawagdy2228@gamil.com>
2026-07-26 22:45:04 +0300

    refactored ScanStatus to Scan/Data/DTOs

---
+++ NutriScan/NutriScan/Features/Scan/Data/DTO/ScanStatus.swift
@@ -0,0 +1,12 @@
+//
+//  ScanStatus.swift
+//  NutriScan
+//
+//  Created by Mina_Wagdy on 26/07/2026.
+//
+
+enum ScanStatus: String, Codable {
+    case processing = "PROCESSING"
+    case completed = "COMPLETED"
+    case failed = "FAILED"
+}
