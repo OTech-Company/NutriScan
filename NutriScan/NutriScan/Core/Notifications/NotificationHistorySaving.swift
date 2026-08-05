3daa80af567b2e2d2be72ae94f52981d1e2130d1
A7mednage71 <ahmdnajkh71@gmail.com>
2026-08-05 19:22:23 +0300

    feat(core): add NotificationHistorySaving protocol and NotificationHistoryItem entity

---
+++ NutriScan/NutriScan/Core/Notifications/NotificationHistorySaving.swift
@@ -0,0 +1,12 @@
+//
+//  NotificationHistorySaving.swift
+//  NutriScan
+//
+//  Created by Ahmed Nageh on 05/08/2026.
+//
+
+import Foundation
+
+protocol NotificationHistorySaving {
+    func saveItem(_ item: NotificationHistoryItem)
+}
