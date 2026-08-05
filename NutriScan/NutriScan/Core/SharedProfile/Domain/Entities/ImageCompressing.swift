94d31410d46e137d753dc731f3be48c435be5a39
Mina_Wagdy <minawagdy2228@gamil.com>
2026-08-06 01:40:56 +0300

    added image compressor to decrease the size of the image data uploaded to avoid bloating the server and delaying the req resulting in req timeout

---
+++ NutriScan/NutriScan/Core/SharedProfile/Domain/Entities/ImageCompressing.swift
@@ -0,0 +1,12 @@
+//
+//  ImageCompressing.swift
+//  NutriScan
+//
+//  Created by Mina_Wagdy on 06/08/2026.
+//
+
+import Foundation
+
+protocol ImageCompressing {
+    func compress(_ data: Data) -> Data
+}
